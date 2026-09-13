d9b34748b4f93f6113824da27f7ebe8d9441a61c
kavyaram03 <kavya06ramachandran@gmail.com>
2026-09-13 17:49:42 +0530

    Updated search functionality

---
+++ product.js
@@ -0,0 +1,40 @@
+//Selecting Side Navbar, menuIcon
+
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0
+})
+
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
+})
+
+
+//Product Search Functionality
+
+var productContainer = document.getElementById("productContainer")
+
+
+var search = document.getElementById("search")
+
+var productList = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup", function (event) {
+    var enteredValue = event.target.value.toUpperCase()
+
+
+    for (count = 0; count < productList.length; count = count + 1) {
+
+        var productname = productList[count].querySelector("h1").textContent
+
+
+        if (productname.toUpperCase().indexOf(enteredValue) < 0) {
+            productList[count].style.display = "none"
+        }
+        else {
+            productList[count].style.display = "block"
+        }
+    }
+})
